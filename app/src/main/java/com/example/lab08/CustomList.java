d59f6d69b4ffe4676f93d7f20940decf96fb9b60
Raza Hamid <raza.hamid2003@gmail.com>
2026-03-14 12:54:28 +0500

    Implement countCities

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -22,6 +22,7 @@ public class CustomList {
         cities.remove(city);
     }
 
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public int countCities() {
+        return cities.size();
+    }
 }
